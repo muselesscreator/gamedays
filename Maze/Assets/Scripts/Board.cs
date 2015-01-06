b9d7cc578fd23d15c455aadcedb641eaf52c8250
Ben Warzeski <bwarzesk@gmail.com>
2015-01-06 15:12:14 -0500

    simple navigation

--- Maze/Assets/Scripts/Board.cs
+++ Maze/Assets/Scripts/Board.cs
@@ -25,17 +25,6 @@ public class Board : MonoBehaviour {
 	
 	}
 
-	void makeWall(float x, float z, Vector3 rotation) {
-		for (int i=0; i<wallHeight; i++) {
-			GameObject newPanel = GameObject.Instantiate (panel) as GameObject;
-			float newY = size/2f + i*size;
-			Debug.Log(newY);
-			newPanel.transform.parent = gameObject.transform;
-			newPanel.transform.localPosition = new Vector3(x, newY, z);
-			newPanel.transform.localRotation = Quaternion.Euler(rotation);
-		}
-	}
-
 	// Update is called once per frame
 	void Update () {
 		if (activate) {
@@ -44,23 +33,16 @@ public class Board : MonoBehaviour {
 			foreach (GameObject thisPanel in panels) {
 				DestroyImmediate (thisPanel);
 			}
-			Vector3 rotation = new Vector3(0, 0, 90);
 			for (int i=0; i<rows; i++) {
 				float newX = -size*cols/2f;
 				float newZ = size*(i+(1-rows)/2f);
-				makeWall (newX, newZ, rotation);
 				for (int j=0; j<cols; j++) {
 					GameObject newPanel = GameObject.Instantiate(panel) as GameObject;
 					newPanel.transform.parent = gameObject.transform;
 					newPanel.transform.localPosition = new Vector3(size*((1-cols)/2f + j), 0, size*((1-rows)/2f + i));
 				}
 			}
-			rotation = new Vector3(90, 0, 0);
-			for (int j=0; j<cols; j++) {
-				float newZ = -size*rows/2f;
-				float newX = size*(j+(1-cols)/2f);
-				makeWall (newX, newZ, rotation);
-			}
+
 		}
 	}
 }
---
+++ Maze/Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int[] position = new int[2] {0,0};
+	public Board theBoard;
+	public bool isWalking = false;
+	public Vector3 targetPosition;
+	Animator anim;
+	public float speed;
+	public float startTime;
+	public float journeyLength;
+	// Use this for initialization
+	
+	void Start () {
+		theBoard = GameObject.Find ("GameController").GetComponent<Board>();
+		anim = GetComponent<Animator>();
+		journeyLength = theBoard.size;
+	}
+
+	void Move (float h, float v) {
+		Debug.Log (h + " " + v);
+
+		if (isWalking == false && Math.Abs (h+v) > 0) {
+			startTime = Time.time;
+
+			anim.SetBool("isWalking", true);
+
+			if (Math.Abs(h) > 0f) {
+				h = -h;
+				if (position[0] - h >= 0 && position[0] - h < theBoard.rows) {
+					isWalking = true;
+					targetPosition.Set(h, 0f, 0f);
+					position[0] -= (int) h;
+				}
+			}
+
+			else if (Math.Abs (v) > 0f) {
+				if (position[1] + v >= 0 && position[1] + v < theBoard.cols) {
+					isWalking = true;
+					targetPosition.Set(0f, 0f, v);
+					position[1] += (int) v;
+				}
+			}
+			targetPosition *= theBoard.size;
+			targetPosition += transform.position;
+		}
+		if (isWalking) {
+			if (transform.position == targetPosition) {
+				anim.SetBool ("isWalking", false);
+				isWalking = false;
+			}
+			else {
+				float distCovered = (Time.time - startTime) * speed;
+				float fracJourney = distCovered / journeyLength;
+				transform.position = Vector3.Lerp (transform.position, targetPosition, fracJourney);
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+		Debug.Log (h + " " + v);
+
+		Move (h, v);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
