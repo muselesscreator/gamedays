96f33d15abba6df421cfc7102714e2712944194b
Ben Warzeski <bwarzesk@gmail.com>
2015-02-18 21:20:31 -0500

    vr stuff

---
+++ Maze/Assets/vrFlyingController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class vrFlyingController : MonoBehaviour {
+
+	GameObject thePlayer;
+	public Vector3 velocity = Vector3.zero;
+	public Vector3 acceleration = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Math.Abs(Input.GetAxis ("Horizontal")) != 0) {
+
+		}
+	}
+}
